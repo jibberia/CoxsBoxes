37de403a76db6871a2bfe36d49cca4e0d0adfc00
Kevin Cox <kevintcox@gmail.com>
2017-01-13 07:56:19 -0500

    Crude VR detector for OS X

---
+++ Assets/VRDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//#if UNITY_EDITOR_OSX
+//// no VR
+//#define NO_VR
+//#endif
+
+public class VRDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		try {
+			Valve.VR.OpenVR.GetInitToken();
+		} catch (DllNotFoundException e) {
+			print("Disabling VR");
+			DisableVR();
+		}
+	}
+
+	void DisableVR() {
+		var vrCam = GameObject.Find("Camera (head)");
+		vrCam.SetActive(false);
+
+	}
+}
